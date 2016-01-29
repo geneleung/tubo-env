0d6ef42a04f93a2f858b328cbf7d80a3fc905f92
yangyingchao <yangyingchao@gmail.com>
2016-01-29 19:08:06 +0800

    removed dead code.

--- .emacs.d/rc/99-proj.el
+++ .emacs.d/rc/99-proj.el
@@ -8,28 +8,6 @@
 
 (require 'cl)
 
-;; This is not correct....
-(defmacro* def-proj (&key
-                     (name nil)
-                     (path nil)
-                     (file nil)
-                     (spp-files nil)
-                     (inc-dirs nil)
-                     (sys-inc-dirs nil)
-                     (spp-defs nil))
-  "Define projects."
-  (list
-   ede-cpp-root-project  `,(or name (file-name-base path))
-    :file         `,(concat path "/" file)
-    :include-path `,inc-dirs
-    :spp-files    `,(mapcar (lambda (x) (concat path x)) spp-files
-                            ;; (remove-if (lambda (x)
-                            ;;              (not (file-exists-p x)) spp-files))
-                            )
-    :system-include-path  `,(mapcar (lambda (x) (concat path x)) sys-inc-dirs)
-    ;; :spp-table             `,spp-defs
-    ))
-
 (dolist (fn (directory-files "~/.emacs.d/rc/" nil "^99[0-9]+.*?\.el"))
   (condition-case msg
       (load-library fn)
