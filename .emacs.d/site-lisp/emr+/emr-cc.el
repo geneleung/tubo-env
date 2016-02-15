6eade52ebad8f85bf8e846b949491246eada4611
yangyingchao <yangyingchao@gmail.com>
2016-02-15 09:30:46 +0800

    feat: updated emr since some changes have been merged to upstream.

--- .emacs.d/site-lisp/emr+/emr-cc.el
+++ .emacs.d/site-lisp/emr+/emr-cc.el
@@ -26,76 +26,6 @@
 ;;; Code:
 
 (require 'emr)
-(require 's)
-(require 'dash)
-(require 'thingatpt)
-(autoload 'c-mode-map "cc-mode")
-
-(defcustom emr-clang-format-style 'Google
-  "Style used to format codes with clang.
-Refer to http://clang.llvm.org/docs/ClangFormatStyleOptions.html for more
-detailed descriptions."
-  :type '(radio (const :tag "Format with style suggested by Google." Google)
-                (const :tag "Format used by LLVM project." LLVM)
-                (const :tag "Format used by Chromium project." Chromium)
-                (const :tag "Format used by Mozilla project." Mozilla)
-                (const :tag "Format used by Webkit project." WebKit)
-                (const :tag "Load style configuration from file." file)
-                (repeat :tag "Customized alist." (cons (regexp :tag "Tag")
-                               (directory :tag "Format"))))
-
-  :group 'emr)
-
-;;; EMR Declarations
-
-(autoload 'clang-format-region "clang-format" ""  t)
-(autoload 'clang-format-buffer "clang-format" ""  t)
-
-(defun emr-cc-get-style ()
-  "Return style as a string."
-  (cond
-   ((stringp emr-clang-format-style) emr-clang-format-style)
-   ((listp emr-clang-format-style)
-    (concat "{"(mapconcat (lambda (x)
-                            (format "%s: %s" (car x) (cdr x)))
-                          emr-clang-format-style ", ") "}"))
-   ((symbolp emr-clang-format-style) (symbol-name emr-clang-format-style))
-   (t nil)))
-
-(defun emr-cc-format-region (start end)
-  "Format region (START/END) using clang."
-  (interactive "rp")
-  (clang-format-region start end (emr-cc-get-style)))
-
-(defun emr-cc-format-buffer ()
-  "Format region (START/END) using clang."
-  (interactive)
-  (clang-format-buffer (emr-cc-get-style)))
-
-(defalias 'emr-cc-tidy-includes 'emr-c-tidy-includes)
-(emr-declare-command 'emr-cc-tidy-includes
-  :title "tidy"
-  :description "includes"
-  :modes '(c++-mode c-mode)
-  :predicate (lambda ()
-               (emr-c:looking-at-include?)))
-
-(emr-declare-command 'emr-cc-format-region
-  :title "format region"
-  :description "with clang"
-  :modes '(c-mode c++-mode)
-  :predicate (lambda ()
-               (and mark-active (not (equal (mark) (point)))
-                    (executable-find "clang-format"))))
-
-(emr-declare-command 'emr-cc-format-buffer
-  :title "format buffer"
-  :description "with clang"
-  :modes '(c-mode c++-mode)
-  :predicate (lambda ()
-               (and (not mark-active)
-                    (executable-find "clang-format"))))
-
 
 (defvar emr-cc-surround-var nil "Nil.")
 
@@ -118,7 +48,7 @@ detailed descriptions."
 (emr-declare-command 'emr-cc-surround-if-end
   :title "surround"
   :description "with if-endif"
-  :modes '(c++-mode)
+  :modes '(c++-mode c-mode)
   :predicate (lambda ()
                (and mark-active (not (equal (mark) (point))))))
 (provide 'emr-cc)
